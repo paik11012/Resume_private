05c52b3a773a9d54c65676b1b2600a67dac33d23
paik11012 <minju11012@gmail.com>
2020-02-17 17:55:40 +0900

    admin page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -8,6 +8,7 @@ import MyPage from './views/MyPage.vue'
 import MainPage from './views/MainPage.vue'
 import SignupPage from './views/SignupPage.vue'
 import UserInfo from './views/UserInfo.vue'
+import AdminPage from './views/AdminPage.vue'
 
 Vue.use(Router)
 
@@ -55,6 +56,11 @@ export default new Router({
 			path: '/userinfo',
 			name: 'userinfo',
 			component: UserInfo
+		},
+		{
+			path: '/admin',
+			name: 'admin',
+			component: AdminPage
 		}
   ]
 })
