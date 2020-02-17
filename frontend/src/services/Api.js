fb6713f251a0054485528a572a0f851a2c3fb930
daseul.lee <0909daseul@gmail.com>
2020-02-17 17:44:57 +0900

    add page to edit user info in vue

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-		// baseURL: 'http://70.12.247.99:8080',
-		baseURL : 'http://15.164.244.244:8080',
+		baseURL: 'http://localhost:8080',
+		// baseURL : 'http://15.164.244.244:8080',
 		withCredentials: false,
 		headers: {
 			'Accept': 'application/json',
