d06e4780ed42c8101e62b74440d12f3d65926a68
kwoneyng <nan308@naver.com>
2020-02-18 14:40:26 +0900

    z-index solve

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-<<<<<<< HEAD
-		// baseURL: 'http://70.12.247.99:8080',
-		baseURL : 'http://15.164.244.244:8080',
-=======
 		baseURL: 'http://70.12.247.99:8080',
 		//baseURL : 'http://15.164.244.244:8080', //회원가입용  url
->>>>>>> 50688dd93665ef7db098ec85963d93168c80f5de
 		withCredentials: false,
 		headers: {
 			'Accept': 'application/json',
